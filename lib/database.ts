a1a20dcd4289066708c3089198b4c731b79fce97
Oscar Alhuay <oscar.a.alhuay@gmail.com>
2026-03-07 14:03:49 -0300

    Configurado Prettier como formateador por defecto y auto-format al guardar

--- lib/database.ts
+++ lib/database.ts
@@ -3,15 +3,15 @@ import { Rol } from '@/types/database'
 
 // Este objeto centraliza todas las acciones de la base de datos
 export const db = {
-  // Función para guardar el rol del usuario
-  async asignarRol(userId: string, rol: Rol) {
-    const { error } = await supabase
-      .from('perfiles')
-      .upsert({ 
-        id: userId, 
-        rol: rol 
-      }) 
-    
-    return { error }
-  }
+    // Función para guardar el rol del usuario
+    async asignarRol(userId: string, rol: Rol) {
+        const { error } = await supabase
+            .from('perfiles')
+            .upsert({
+                id: userId,
+                rol: rol
+            })
+
+        return { error }
+    }
 }
